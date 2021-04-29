0aca44997a26616f94d0d9dbaeb9a00630050141
Onyeka Obi <Onyeka.Obi@gmail.com>
2021-04-29 09:03:54 -0700

    Maximize referential transparency (totality) of mappers

--- client/src/main/kotlin/helloworldclient.kt
+++ client/src/main/kotlin/helloworldclient.kt
@@ -6,20 +6,19 @@ import io.grpc.netty.NettyChannelBuilder
 import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-    val channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build()
-    val blockingStub = GreeterGrpc.newBlockingStub(channel)
-//    val request = HelloRequest.newBuilder().setName("Steve").build()
-    val builder = HelloRequest.newBuilder()
-    with(builder) {
-        name = "Steve"
-    }
-    val request = builder.build()
-    try {
-        println("Calling server")
-        val response = blockingStub.sayHello(request)
-        println("Server called")
-        println("Response from server: ${response.message}")
-    } finally {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
-    }
+  val channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build()
+  val blockingStub = GreeterGrpc.newBlockingStub(channel)
+  val builder = HelloRequest.newBuilder()
+  with(builder) {
+    name = "Steve"
+  }
+  val request = builder.build()
+  try {
+    println("Calling server")
+    val response = blockingStub.sayHello(request)
+    println("Server called")
+    println("Response from server: ${response.message}")
+  } finally {
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+  }
 }
--- new-client/src/main/scala/io/github/mavenrain/HelloWorldClient.scala
+++ new-client/src/main/scala/io/github/mavenrain/HelloWorldClient.scala
@@ -1,6 +1,14 @@
 package io.github.mavenrain
 
-import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+import com.fasterxml.jackson.databind.DeserializationFeature.{
+  FAIL_ON_INVALID_SUBTYPE, FAIL_ON_UNKNOWN_PROPERTIES,
+  FAIL_ON_UNRESOLVED_OBJECT_IDS, READ_UNKNOWN_ENUM_VALUES_AS_NULL
+}
+import com.fasterxml.jackson.databind.SerializationFeature.{
+  CLOSE_CLOSEABLE, FAIL_ON_EMPTY_BEANS, FAIL_ON_SELF_REFERENCES,
+  FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, WRITE_DATES_AS_TIMESTAMPS,
+  WRITE_DURATIONS_AS_TIMESTAMPS
+}
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
@@ -20,8 +28,34 @@ object HelloWorldClient extends App {
   private implicit object ReleasableChannel extends Releasable[ManagedChannel] {
     def release(resource: ManagedChannel): Unit = resource.shutdown().awaitTermination(1, TimeUnit.SECONDS)
   }
-  private val contractMapper = new ObjectMapper().registerModule(new ProtobufModule).configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
-  private val domainMapper = JsonMapper.builder().addModule(DefaultScalaModule).build().configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
+  private val contractMapper =
+    new ObjectMapper()
+      .registerModule(new ProtobufModule)
+      .configure(CLOSE_CLOSEABLE, false)
+      .configure(FAIL_ON_EMPTY_BEANS, false)
+      .configure(FAIL_ON_INVALID_SUBTYPE, false)
+      .configure(FAIL_ON_SELF_REFERENCES, false)
+      .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
+      .configure(FAIL_ON_UNRESOLVED_OBJECT_IDS, false)
+      .configure(FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false)
+      .configure(READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+      .configure(WRITE_DATES_AS_TIMESTAMPS, false)
+      .configure(WRITE_DURATIONS_AS_TIMESTAMPS, false)
+  private val domainMapper =
+    JsonMapper
+      .builder()
+      .addModule(DefaultScalaModule)
+      .build()
+      .configure(CLOSE_CLOSEABLE, false)
+      .configure(FAIL_ON_EMPTY_BEANS, false)
+      .configure(FAIL_ON_INVALID_SUBTYPE, false)
+      .configure(FAIL_ON_SELF_REFERENCES, false)
+      .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
+      .configure(FAIL_ON_UNRESOLVED_OBJECT_IDS, false)
+      .configure(FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false)
+      .configure(READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+      .configure(WRITE_DATES_AS_TIMESTAMPS, false)
+      .configure(WRITE_DURATIONS_AS_TIMESTAMPS, false)
   private def mapToDomain[S, T](request: S, reference: Class[T]): T =
     request
       .pipe(contractMapper.writeValueAsString(_))
